08a02f7e24bbaa45b644143d5225145c472a21c7
tramcandoit <22521497@gm.uit.edu.vn>
2024-05-07 01:29:47 +0700

    test workflow 2

--- client/pages/index.js
+++ client/pages/index.js
@@ -17,7 +17,7 @@ const LandingPage = ({ currentUser, tickets }) => {
 
   return (
     <div>
-      <h1>Danh sách</h1>
+      <h1>Buồn ngủ quá</h1>
       <table className="table">
         <thead>
           <tr>
