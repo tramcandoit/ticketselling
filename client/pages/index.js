aae2ffabb9b2fd75eef503313b429bcfb28dc532
tramcandoit <22521497@gm.uit.edu.vn>
2024-05-07 02:08:33 +0700

    test 8

--- client/pages/index.js
+++ client/pages/index.js
@@ -17,7 +17,7 @@ const LandingPage = ({ currentUser, tickets }) => {
 
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Help</h1>
       <table className="table">
         <thead>
           <tr>
