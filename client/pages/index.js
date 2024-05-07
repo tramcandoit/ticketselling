9d82dc7b263df5f8c3992eaf414231a1376eba43
tramcandoit <22521497@gm.uit.edu.vn>
2024-05-07 13:52:35 +0700

    update

--- client/pages/index.js
+++ client/pages/index.js
@@ -17,7 +17,7 @@ const LandingPage = ({ currentUser, tickets }) => {
 
   return (
     <div>
-      <h1> Tram </h1>
+      <h1> Ngọc Trăm </h1>
       <table className="table">
         <thead>
           <tr>
